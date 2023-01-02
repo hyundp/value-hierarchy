b1b5bddc3839efb155726252433c529d43f343cf
dyun <zkdlxkxks@nate.com>
2023-01-02 23:51:06 +0900

    start : hierarchySystem class

---
+++ HierarchySystem.py
@@ -0,0 +1,11 @@
+import Value
+
+class HierarchySystem():
+    def __init__(self):
+        cnt = Value.get_value_count()
+
+
+    def set_value_weight():
+        value_list = Value.get_value_list()
+        for value in value_list:
+            value
