17dba01e9334bff7e146d6b3087b20f86fced225
dyun <zkdlxkxks@nate.com>
2023-02-05 00:17:15 +0900

    update: value weight

--- Value.py
+++ Value.py
@@ -5,16 +5,21 @@ class Value():
         -> str : float
         """
         value_dict = {
-            '사랑' : 0,
-            '자산': 0,
-            '능력' : 0,
-            '건강' : 0,
-            '안정' : 0,
-            '즐거움' : 0,
-            '자유' : 0,
-            '책임' : 0,
-            '현재' : 0,
-            '미래' : 0,
+            # 그 일은 나에게 어떤 가치를 주는가? (0.5)
+            '사랑' : 8,
+            '자산': 9,
+            '능력' : 10,
+            '건강' : 7,
+            '안정' : 6,
+            '즐거움' : 4,
+            '자유' : 5,
+            '책임' : 6,
+        }
+
+        time_dict = {
+            # 그 일은 어떤 시점에 가치를 주는가?
+            '현재' : 6,
+            '미래' : 10,
         }
 
     def get_value_count(self):
@@ -23,3 +28,9 @@ class Value():
 
     def get_value_name(self):
         return list(self.value_dict.keys)
+
+    def get_value_dict(self):
+        return self.value_dict
+
+    def get_time_dict(self):
+        return self.time_dict
