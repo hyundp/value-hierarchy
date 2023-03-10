f56925f71ff744076799d9ed1befd7ea40e85a39
dyun <zkdlxkxks@nate.com>
2023-03-11 01:52:50 +0900

    add: 선택지 + 점수결과값

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import HierarchySystem
+from HierarchySystem import HierarchySystem
+
+choice = input("선택지를 설명해주세요")
 
 hierarchy = HierarchySystem()
+
+input_val = hierarchy.set_input_val()
+input_time = hierarchy.set_input_time()
+result = hierarchy.cal_obj_function(input_val, input_time)
+
+print(f"{choice}의 점수는 \n {result}입니다.")
 
 
